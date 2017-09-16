1ec839f4e00e629123669c589cdd4e5ee86f9aa8
Pat Needham <needham.patrick@gmail.com>
2017-09-16 18:54:17 -0400

    update optionDefinitions, keeping comments within 80 characters on each line

--- optionDefinitions.js
+++ optionDefinitions.js
@@ -1,12 +1,34 @@
 const optionDefinitions = [
-  { name: 'query', alias: 'q', type: String }, // the query that should be sent to the Google search
-  { name: 'output', alias: 'o', type: String }, // name of the JSON file to save results to
-  { name: 'save', alias: 's', type: String }, // name of the html file if you want to save the actual response from the html request
-  { name: 'limit', alias: 'l', type: Number }, // number of search results to be returned
-  { name: 'verbose', alias: 'v', type: Boolean }, // console.log useful statements to show what's currently taking place
-  { name: 'interactive', alias: 'i', type: Boolean }, // once results are returned, show them in an interactive prompt where user can scroll through them
-  { name: 'bold-matching-text', alias: 'b', type: Boolean}, // only takes effect when interactive (-i) flag is set as well, will bold test in results that matched the query
-  { name: 'stackoverflow-github-only', alias: 'X', type: Boolean } // option to limit results to only these two sites
+  // the query that should be sent to the Google search
+  { name: 'query', alias: 'q', type: String },
+
+  // name of the JSON file to save results to
+  { name: 'output', alias: 'o', type: String },
+
+  // prevent results from appearing in the terminal output. Should only be used
+  // with --output (-o) command when saving results to a file
+  { name: 'no-display', alias: 'n', type: Boolean},
+
+  // name of the html file if you want to save the actual response from the
+  // html request
+  { name: 'save', alias: 's', type: String },
+
+  // number of search results to be returned
+  { name: 'limit', alias: 'l', type: Number },
+
+  // console.log useful statements to show what's currently taking place
+  { name: 'verbose', alias: 'v', type: Boolean },
+
+  // once results are returned, show them in an interactive prompt where user
+  // can scroll through them
+  { name: 'interactive', alias: 'i', type: Boolean },
+
+  // only takes effect when interactive (-i) flag is set as well, will bold
+  // test in results that matched the query
+  { name: 'bold-matching-text', alias: 'b', type: Boolean},
+
+  // option to limit results to only these two sites
+  { name: 'stackoverflow-github-only', alias: 'X', type: Boolean }
 ]
 
 module.exports = optionDefinitions
